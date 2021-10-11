4ba49dc40270b851619d87c50c26cc3b1d3bc4e9
marioCST <marioCST@noreply.github.com>
2021-10-11 19:19:51 +0200

    It's looking nice

--- src/main/java/de/mariocst/cc/listeners/ServerListPingListener.java
+++ src/main/java/de/mariocst/cc/listeners/ServerListPingListener.java
@@ -1,6 +1,10 @@
 package de.mariocst.cc.listeners;
 
 import com.destroystokyo.paper.event.server.PaperServerListPingEvent;
+import de.mariocst.cc.CCPlugin;
+import de.mariocst.cc.utils.FakePlayerProfile;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -8,5 +12,17 @@ public class ServerListPingListener implements Listener {
     @EventHandler
     public void onPing(PaperServerListPingEvent event) {
         event.setVersion("Minecraft 1.9.x - 1.17.x");
+
+        int players = 0;
+
+        for (Player ignored : CCPlugin.getInstance().getServer().getOnlinePlayers()) {
+            players++;
+        }
+
+        event.setNumPlayers(players);
+
+        event.getPlayerSample().clear();
+        event.getPlayerSample().add(new FakePlayerProfile("\u00A73\u00A7lCity\u00A79\u00A7lCraft"));
+        event.getPlayerSample().add(new FakePlayerProfile("\u00A7b\u00A7l1.9.x - 1.17.x"));
     }
 }
