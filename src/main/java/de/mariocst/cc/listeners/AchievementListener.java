9f7243a58117ec9bf256874eff77e9145fe84fdf
marioCST <marioCST@noreply.github.com>
2021-10-15 15:31:43 +0200

    Probably fixed the empty message Bug

--- src/main/java/de/mariocst/cc/listeners/AchievementListener.java
+++ src/main/java/de/mariocst/cc/listeners/AchievementListener.java
@@ -1,6 +1,5 @@
 package de.mariocst.cc.listeners;
 
-import net.kyori.adventure.text.Component;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerAdvancementDoneEvent;
@@ -8,6 +7,6 @@ import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 public class AchievementListener implements Listener {
     @EventHandler
     public void onAchievement(PlayerAdvancementDoneEvent event) {
-        event.message(Component.text(""));
+        event.message(null);
     }
 }
